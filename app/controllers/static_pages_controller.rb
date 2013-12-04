9cb45f3a96c204a69aea6363e93711755bf30925
monchhichi678 <lyclaugh@outlook.com>
2013-12-04 21:26:24 +0800

    fix some problems in signup view

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+	
+  def contact
+  end
+
+  def about
+  end 
+  
+  def signin	
+  end
+
+  def signup
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+def full_title(page_title)
+base_title="Tucao"
+	if page_title.empty?
+	base_title
+	else
+	"#{base_title}|#{page_title}"
+	end
+      end
+   end
